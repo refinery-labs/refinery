2873043cae9c789bc41a21add27668b9a9f0bd66
Ubuntu <ubuntu@ip-172-31-41-45.us-west-2.compute.internal>
2019-02-17 03:31:22 +0000

    Added www.refinerylabs.io, commit before adding Node support

---
+++ www/build.sh
@@ -0,0 +1,11 @@
+echo "Building site..."
+jekyll build
+
+rm -rf ./_site/build.sh
+rm -rf ./_site/env/
+rm -rf ./_site/serve.sh
+rm -rf ./_site/LICENSE.txt
+rm -rf ./_site/README.md
+
+echo "Syncing site to S3..."
+s3cmd sync --acl-public --delete-removed ./_site/ s3://www.refinerylabs.io/
