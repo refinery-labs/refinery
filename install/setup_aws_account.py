895f85803daf17c09bac120bc2c2124e1d5de533
Free Wortley <00free@gmail.com>
2019-03-06 22:00:39 -0800

    Remove idea file stuff -- it's noisy. And reformat file with spaces. And split up a function

--- install/setup_aws_account.py
+++ install/setup_aws_account.py
@@ -31,220 +31,220 @@ IAM_CLIENT = boto3.client(
 )
 
 def clear_previous_iam():
-	paired_iams = [
-		{
-			"role_name": "refinery_aws_lambda_admin_role",
-			"policy_arn": "arn:aws:iam::" + os.environ.get( "aws_account_id" ) + ":policy/refinery_aws_lambda_admin_policy",
-		},
-		{
-			"role_name": "refinery_aws_cloudwatch_admin_role",
-			"policy_arn": "arn:aws:iam::" + os.environ.get( "aws_account_id" ) + ":policy/refinery_aws_cloudwatch_admin_policy",
-		}
-	]
-	
-	
-	for paired_iam in paired_iams:
-		print( "Deleting role '" + paired_iam[ "role_name" ] + "' and its associated policies..." )
-		try:
-			IAM_CLIENT.detach_role_policy(
-				RoleName=paired_iam[ "role_name" ],
-				PolicyArn=paired_iam[ "policy_arn" ]
-			)
-			
-			IAM_CLIENT.delete_role(
-				RoleName=paired_iam[ "role_name" ]
-			)
-			
-			IAM_CLIENT.delete_policy(
-				PolicyArn=paired_iam[ "policy_arn" ]
-			)
-		except:
-			print( "Error deleting policy, may not already exist? Continuing...")
-		
+    paired_iams = [
+        {
+            "role_name": "refinery_aws_lambda_admin_role",
+            "policy_arn": "arn:aws:iam::" + os.environ.get( "aws_account_id" ) + ":policy/refinery_aws_lambda_admin_policy",
+        },
+        {
+            "role_name": "refinery_aws_cloudwatch_admin_role",
+            "policy_arn": "arn:aws:iam::" + os.environ.get( "aws_account_id" ) + ":policy/refinery_aws_cloudwatch_admin_policy",
+        }
+    ]
+
+
+    for paired_iam in paired_iams:
+        print( "Deleting role '" + paired_iam[ "role_name" ] + "' and its associated policies..." )
+        try:
+            IAM_CLIENT.detach_role_policy(
+                RoleName=paired_iam[ "role_name" ],
+                PolicyArn=paired_iam[ "policy_arn" ]
+            )
+
+            IAM_CLIENT.delete_role(
+                RoleName=paired_iam[ "role_name" ]
+            )
+
+            IAM_CLIENT.delete_policy(
+                PolicyArn=paired_iam[ "policy_arn" ]
+            )
+        except:
+            print( "Error deleting policy, may not already exist? Continuing...")
+
 def setup_lambda_iam():
-	iam_role_name = "refinery_aws_lambda_admin_role"
-	iam_policy_name = "refinery_aws_lambda_admin_policy"
-	
-	print( "Creating Lambda IAM policy..." )
-	create_policy_response = IAM_CLIENT.create_policy(
-		PolicyName=iam_policy_name,
-		PolicyDocument=json.dumps({
-		"Version": "2012-10-17",
-		"Statement": [
-				{
-					"Action": "*",
-					"Effect": "Allow",
-					"Resource": "*"
-				}
-			]
-		}),
-		Description="refinery AWS Lambda admin runtime IAM policy."
-	)
-	iam_policy_arn = create_policy_response[ "Policy" ][ "Arn" ]
-	print( "Policy ARN: " + iam_policy_arn )
-	
-	print( "Creating IAM role..." )
-	create_role_response = IAM_CLIENT.create_role(
-		RoleName=iam_role_name,
-		AssumeRolePolicyDocument=json.dumps({
-			"Version": "2012-10-17",
-			"Statement": [
-				{
-					"Effect": "Allow",
-					"Principal": {
-						"Service": "lambda.amazonaws.com"
-					},
-					"Action": "sts:AssumeRole"
-				},
-				{
-					"Effect": "Allow",
-					"Principal": {
-						"Service": "events.amazonaws.com"
-					},
-					"Action": "sts:AssumeRole"
-				},
-				{
-					"Effect": "Allow",
-					"Principal": {
-						"Service": "s3.amazonaws.com"
-					},
-					"Action": "sts:AssumeRole"
-				}
-			]
-		}),
-		Description="refinery AWS Lambda admin runtime IAM role.",
-	)
-	
-	iam_role_arn = create_role_response[ "Role" ][ "Arn" ]
-	
-	print( "Attaching IAM policy to role..." )
-	attach_role_policy_response = IAM_CLIENT.attach_role_policy(
-		RoleName=iam_role_name,
-		PolicyArn=iam_policy_arn
-	)
-	
-	return iam_role_arn
+    iam_role_name = "refinery_aws_lambda_admin_role"
+    iam_policy_name = "refinery_aws_lambda_admin_policy"
+
+    print( "Creating Lambda IAM policy..." )
+    create_policy_response = IAM_CLIENT.create_policy(
+        PolicyName=iam_policy_name,
+        PolicyDocument=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "*",
+                    "Effect": "Allow",
+                    "Resource": "*"
+                }
+            ]
+        }),
+        Description="refinery AWS Lambda admin runtime IAM policy."
+    )
+    iam_policy_arn = create_policy_response[ "Policy" ][ "Arn" ]
+    print( "Policy ARN: " + iam_policy_arn )
+
+    print( "Creating IAM role..." )
+    create_role_response = IAM_CLIENT.create_role(
+        RoleName=iam_role_name,
+        AssumeRolePolicyDocument=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "lambda.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                },
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "events.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                },
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "s3.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }),
+        Description="refinery AWS Lambda admin runtime IAM role.",
+    )
+
+    iam_role_arn = create_role_response[ "Role" ][ "Arn" ]
+
+    print( "Attaching IAM policy to role..." )
+    attach_role_policy_response = IAM_CLIENT.attach_role_policy(
+        RoleName=iam_role_name,
+        PolicyArn=iam_policy_arn
+    )
+
+    return iam_role_arn
 
 def setup_events_iam():
-	iam_role_name = "refinery_aws_cloudwatch_admin_role"
-	iam_policy_name = "refinery_aws_cloudwatch_admin_policy"
-	
-	print( "Creating Events IAM policy..." )
-	create_policy_response = IAM_CLIENT.create_policy(
-		PolicyName=iam_policy_name,
-		PolicyDocument=json.dumps({
-			"Version": "2012-10-17",
-			"Statement": [
-				{
-					"Effect": "Allow",
-					"Action": [
-						"cloudformation:DescribeChangeSet",
-						"cloudformation:DescribeStackResources",
-						"cloudformation:DescribeStacks",
-						"cloudformation:GetTemplate",
-						"cloudformation:ListStackResources",
-						"cloudwatch:*",
-						"cognito-identity:ListIdentityPools",
-						"cognito-sync:GetCognitoEvents",
-						"cognito-sync:SetCognitoEvents",
-						"dynamodb:*",
-						"ec2:DescribeSecurityGroups",
-						"ec2:DescribeSubnets",
-						"ec2:DescribeVpcs",
-						"events:*",
-						"iam:GetPolicy",
-						"iam:GetPolicyVersion",
-						"iam:GetRole",
-						"iam:GetRolePolicy",
-						"iam:ListAttachedRolePolicies",
-						"iam:ListRolePolicies",
-						"iam:ListRoles",
-						"iam:PassRole",
-						"iot:AttachPrincipalPolicy",
-						"iot:AttachThingPrincipal",
-						"iot:CreateKeysAndCertificate",
-						"iot:CreatePolicy",
-						"iot:CreateThing",
-						"iot:CreateTopicRule",
-						"iot:DescribeEndpoint",
-						"iot:GetTopicRule",
-						"iot:ListPolicies",
-						"iot:ListThings",
-						"iot:ListTopicRules",
-						"iot:ReplaceTopicRule",
-						"kinesis:DescribeStream",
-						"kinesis:ListStreams",
-						"kinesis:PutRecord",
-						"kms:ListAliases",
-						"lambda:*",
-						"logs:*",
-						"s3:*",
-						"sns:ListSubscriptions",
-						"sns:ListSubscriptionsByTopic",
-						"sns:ListTopics",
-						"sns:Publish",
-						"sns:Subscribe",
-						"sns:Unsubscribe",
-						"sqs:ListQueues",
-						"sqs:SendMessage",
-						"tag:GetResources",
-						"xray:PutTelemetryRecords",
-						"xray:PutTraceSegments"
-					],
-					"Resource": "*"
-				}
-			]
-		}),
-		Description="refinery AWS CloudWatch admin runtime IAM policy."
-	)
-	iam_policy_arn = create_policy_response[ "Policy" ][ "Arn" ]
-	
-	print( "Policy ARN: " + iam_policy_arn )
-	
-	print( "Creating IAM role..." )
-	create_role_response = IAM_CLIENT.create_role(
-		RoleName=iam_role_name,
-		AssumeRolePolicyDocument=json.dumps({
-			"Version": "2012-10-17",
-			"Statement": [
-				{
-					"Effect": "Allow",
-					"Principal": {
-						"Service": "events.amazonaws.com"
-					},
-					"Action": "sts:AssumeRole"
-				}
-			]
-		}),
-		Description="refinery AWS CloudWatch admin runtime IAM role.",
-	)
-	
-	iam_role_arn = create_role_response[ "Role" ][ "Arn" ]
-	
-	print( "Attaching IAM policy to role..." )
-	attach_role_policy_response = IAM_CLIENT.attach_role_policy(
-		RoleName=iam_role_name,
-		PolicyArn=iam_policy_arn
-	)
-	
-	return iam_role_arn
-	
+    iam_role_name = "refinery_aws_cloudwatch_admin_role"
+    iam_policy_name = "refinery_aws_cloudwatch_admin_policy"
+
+    print( "Creating Events IAM policy..." )
+    create_policy_response = IAM_CLIENT.create_policy(
+        PolicyName=iam_policy_name,
+        PolicyDocument=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "cloudformation:DescribeChangeSet",
+                        "cloudformation:DescribeStackResources",
+                        "cloudformation:DescribeStacks",
+                        "cloudformation:GetTemplate",
+                        "cloudformation:ListStackResources",
+                        "cloudwatch:*",
+                        "cognito-identity:ListIdentityPools",
+                        "cognito-sync:GetCognitoEvents",
+                        "cognito-sync:SetCognitoEvents",
+                        "dynamodb:*",
+                        "ec2:DescribeSecurityGroups",
+                        "ec2:DescribeSubnets",
+                        "ec2:DescribeVpcs",
+                        "events:*",
+                        "iam:GetPolicy",
+                        "iam:GetPolicyVersion",
+                        "iam:GetRole",
+                        "iam:GetRolePolicy",
+                        "iam:ListAttachedRolePolicies",
+                        "iam:ListRolePolicies",
+                        "iam:ListRoles",
+                        "iam:PassRole",
+                        "iot:AttachPrincipalPolicy",
+                        "iot:AttachThingPrincipal",
+                        "iot:CreateKeysAndCertificate",
+                        "iot:CreatePolicy",
+                        "iot:CreateThing",
+                        "iot:CreateTopicRule",
+                        "iot:DescribeEndpoint",
+                        "iot:GetTopicRule",
+                        "iot:ListPolicies",
+                        "iot:ListThings",
+                        "iot:ListTopicRules",
+                        "iot:ReplaceTopicRule",
+                        "kinesis:DescribeStream",
+                        "kinesis:ListStreams",
+                        "kinesis:PutRecord",
+                        "kms:ListAliases",
+                        "lambda:*",
+                        "logs:*",
+                        "s3:*",
+                        "sns:ListSubscriptions",
+                        "sns:ListSubscriptionsByTopic",
+                        "sns:ListTopics",
+                        "sns:Publish",
+                        "sns:Subscribe",
+                        "sns:Unsubscribe",
+                        "sqs:ListQueues",
+                        "sqs:SendMessage",
+                        "tag:GetResources",
+                        "xray:PutTelemetryRecords",
+                        "xray:PutTraceSegments"
+                    ],
+                    "Resource": "*"
+                }
+            ]
+        }),
+        Description="refinery AWS CloudWatch admin runtime IAM policy."
+    )
+    iam_policy_arn = create_policy_response[ "Policy" ][ "Arn" ]
+
+    print( "Policy ARN: " + iam_policy_arn )
+
+    print( "Creating IAM role..." )
+    create_role_response = IAM_CLIENT.create_role(
+        RoleName=iam_role_name,
+        AssumeRolePolicyDocument=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "events.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }),
+        Description="refinery AWS CloudWatch admin runtime IAM role.",
+    )
+
+    iam_role_arn = create_role_response[ "Role" ][ "Arn" ]
+
+    print( "Attaching IAM policy to role..." )
+    attach_role_policy_response = IAM_CLIENT.attach_role_policy(
+        RoleName=iam_role_name,
+        PolicyArn=iam_policy_arn
+    )
+
+    return iam_role_arn
+
 def create_refinery_buckets():
-	random_ext = str(
-		uuid.uuid4()
-	).replace( "-", "" )
-	
-	lambda_package_bucket_name = "lambdabuildpackages-" + random_ext
-	S3_CLIENT.create_bucket(
-            Bucket=lambda_package_bucket_name,
-            CreateBucketConfiguration={
-                "LocationConstraint": os.environ.get( "aws_region" ),
-            }
-	)
-	
-	lifecycle_response = S3_CLIENT.put_bucket_lifecycle_configuration(
-		Bucket=lambda_package_bucket_name,
-		LifecycleConfiguration={
+    random_ext = str(
+        uuid.uuid4()
+    ).replace( "-", "" )
+
+    lambda_package_bucket_name = "lambdabuildpackages-" + random_ext
+    S3_CLIENT.create_bucket(
+        Bucket=lambda_package_bucket_name,
+        CreateBucketConfiguration={
+            "LocationConstraint": os.environ.get( "aws_region" ),
+        }
+    )
+
+    lifecycle_response = S3_CLIENT.put_bucket_lifecycle_configuration(
+        Bucket=lambda_package_bucket_name,
+        LifecycleConfiguration={
             "Rules": [
                 {
                     "ID": "autoexpire",
@@ -256,30 +256,30 @@ def create_refinery_buckets():
                 }
             ]
         }
-	)
-	
-	print( "Lifecycle response: " )
-	print( lifecycle_response )
+    )
 
-	print( "Lambda package bucket name: " )
-	print( lambda_package_bucket_name )
+    print( "Lifecycle response: " )
+    print( lifecycle_response )
 
-	random_ext = str(
-		uuid.uuid4()
-	).replace( "-", "" )
-	
-	lambda_logging_bucket_name = "lambda-logging-" + random_ext
-	S3_CLIENT.create_bucket(
-            Bucket=lambda_logging_bucket_name,
-            CreateBucketConfiguration={
-                "LocationConstraint": os.environ.get( "aws_region" ),
-            }
-	)
+    print( "Lambda package bucket name: " )
+    print( lambda_package_bucket_name )
 
-	return {
-            "packages_bucket_name": lambda_package_bucket_name,
-            "logging_bucket_name": lambda_logging_bucket_name,
+    random_ext = str(
+        uuid.uuid4()
+    ).replace( "-", "" )
+
+    lambda_logging_bucket_name = "lambda-logging-" + random_ext
+    S3_CLIENT.create_bucket(
+        Bucket=lambda_logging_bucket_name,
+        CreateBucketConfiguration={
+            "LocationConstraint": os.environ.get( "aws_region" ),
         }
+    )
+
+    return {
+        "packages_bucket_name": lambda_package_bucket_name,
+        "logging_bucket_name": lambda_logging_bucket_name,
+    }
 
 bucket_data = create_refinery_buckets()
 clear_previous_iam()
