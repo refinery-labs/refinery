bcbc82e4763cdba60e400271bc743f670c8b243e
Free Wortley <00free@gmail.com>
2019-03-27 00:10:34 -0700

    Clean up config file

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,16 +1,12 @@
 module.exports = {
   lintOnSave: false,
   css: {
-    // modules: true,
     loaderOptions: {
       // pass options to sass-loader
       sass: {
         // @/ is an alias to src/
         // so this assumes you have a file named `src/variables.scss`
         data: `@import "~@/styles/variables.scss";`,
-        // includePaths: [
-        //   path.resolve(__dirname, 'src/styles/')
-        // ],
       }
     }
   }
