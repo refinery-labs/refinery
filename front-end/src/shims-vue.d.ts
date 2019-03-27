799a19233ffab926e71ec963d92628c85b4eedf7
Free Wortley <00free@gmail.com>
2019-03-27 00:07:39 -0700

    Update Vue config and translate the .vue file into pure JS format

--- front-end/src/shims-vue.d.ts
+++ front-end/src/shims-vue.d.ts
@@ -2,3 +2,13 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+
+declare module "@/styles/*.scss" {
+  const styles: any;
+  export = styles;
+}
+
+declare module "@/styles/*.css" {
+  const styles: any;
+  export = styles;
+}
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    // modules: true,
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        // @/ is an alias to src/
+        // so this assumes you have a file named `src/variables.scss`
+        data: `@import "~@/styles/variables.scss";`,
+        // includePaths: [
+        //   path.resolve(__dirname, 'src/styles/')
+        // ],
+      }
+    }
+  }
 };
