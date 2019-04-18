8972c8a2e15cef44c4e117c5f721eb2caad41b18
Free Wortley <00free@gmail.com>
2019-04-18 14:36:27 -0700

    Remove commented out code

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -25,11 +25,6 @@ export default class App extends Vue {
   public render(h: CreateElement): VNode {
     return (
       <div id="app" class="display--flex">
-        {/*<div id="nav">*/}
-        {/*<router-link to="/">Home</router-link> |*/}
-        {/*<router-link to="/about">About</router-link>*/}
-        {/*</div>*/}
-        
         <SidebarNav props={{navItems}}/>
         <div class="flex-grow--3">
           <router-view />
