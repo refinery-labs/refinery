2a5dbc554ea5fb297b1b61c2d45b4e6eb7cb602b
mandatory <ubuntu@ip-172-31-41-45.us-west-2.compute.internal>
2019-05-29 06:44:32 +0000

    ffs

--- api/server.py
+++ api/server.py
@@ -6326,7 +6326,6 @@ class RunMonthlyStripeBillingJob( BaseHandler ):
 		logit( "[ STATUS ] Stripe billing job has completed!" )
 		
 class HealthHandler( BaseHandler ):
-	@authenticated
 	def get( self ):
 		# Just run a dummy database query to ensure it's working
 		session.query( User ).first()
