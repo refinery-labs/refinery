5de5db353e093ab2f606a4e9ed9072533bd37ebb
Matthew Bryant <matt@refinerylabs.io>
2019-06-14 22:35:09 +0000

    Quick commit

--- front-end/src/views/ProjectsNestedViews/ProjectSettings.tsx
+++ front-end/src/views/ProjectsNestedViews/ProjectSettings.tsx
@@ -1,15 +1,33 @@
-import Vue, { CreateElement, VNode } from 'vue';
+import Vue, {CreateElement, VNode} from 'vue';
 import Component from 'vue-class-component';
 
 @Component
 export default class ProjectSettings extends Vue {
   public render(h: CreateElement): VNode {
     return (
-      <div class="project-settings-page">
-        <h2>Project Settings Page</h2>
-        <span>TODO: Add "change project name" feature here</span>
-        <span>TODO: Add "change log level" setting</span>
-        <router-view />
+      <div class="content-wrapper">
+        <div class="content-heading display-flex">
+          <div class="layout--constrain flex-grow--1">
+            <div>Project Settings
+              <small>The settings for this project.</small>
+            </div>
+          </div>
+        </div>
+        <div class="layout--constrain">
+          <div class="row justify-content-lg-center">
+            <div class="col-lg-8 align-self-center">
+              <div class="card card-default">
+                <div class="card-header">
+                  Projects
+                </div>
+
+                <div class="card-footer">
+                  test
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
