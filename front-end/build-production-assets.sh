0006b250759a6798c9653df24d813867fa242ead
Free Wortley <00free@gmail.com>
2019-06-20 21:33:29 -0700

    More build changes

---
+++ front-end/build-production-assets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create the folder where the output files will go
+mkdir front-end-dist
+
+# Build the front-end assets
+docker build -t front-end-image .
+
+# Run the container with a specific npm command and mount our output volume
+docker run -v "$(pwd)"/front-end-dist:/work/output -it front-end-image build-and-copy
+
