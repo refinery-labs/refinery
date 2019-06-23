f6100a961485fe1ec26a985e026dfc02b9e32298
Free Wortley <00free@gmail.com>
2019-06-23 16:30:02 -0700

    Pass through more Env vars

--- front-end/build-production-assets.sh
+++ front-end/build-production-assets.sh
@@ -10,5 +10,8 @@ docker build -t front-end-image .
 docker run \
   -e AWS_DEFAULT_REGION \
   -e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e AWS_SESSION_TOKEN \
   -v "$(pwd)"/front-end-dist:/work/output \
   -t front-end-image build-and-copy
