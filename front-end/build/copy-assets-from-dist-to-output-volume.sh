5baf5b7ecdaa5c87f0a1cbd6eca509c2db044e6a
Free Wortley <00free@gmail.com>
2019-06-23 22:06:37 -0700

    Move builds scripts and proxy to CDN from nginx

---
+++ front-end/build/copy-assets-from-dist-to-output-volume.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r /work/front-end/dist/index.html /work/output/index.html
+cp -r /work/front-end/dist/service-worker.js /work/output/service-worker.js
+cp -r /work/front-end/dist/robots.txt /work/output/robots.txt
+cp -r /work/front-end/dist/favicon.ico /work/output/favicon.ico
