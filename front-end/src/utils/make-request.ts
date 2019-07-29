85e0ea0c3fcbc8f19172cfa104927a4c1b962c3b
Free Wortley <00free@gmail.com>
2019-07-29 12:58:46 -0700

    Fix Typescript error because I am bad with computers

--- front-end/src/utils/make-request.ts
+++ front-end/src/utils/make-request.ts
@@ -146,5 +146,8 @@ export const HttpUtil: HttpMethodLookup = {
   [HTTP_METHOD.OPTIONS]: async (path, body, args?) => await optionsRequest(path, body, args),
   [HTTP_METHOD.HEAD]: async (path, body, args?) => await headRequest(path, body, args),
   [HTTP_METHOD.PATCH]: async (path, body, args?) => await patchRequest(path, body, args),
-  [HTTP_METHOD.PUT]: async (path, body, args?) => await putRequest(path, body, args)
+  [HTTP_METHOD.PUT]: async (path, body, args?) => await putRequest(path, body, args),
+  ANY: async (path, body, args?) => {
+    throw new Error('Not implemented');
+  }
 };
