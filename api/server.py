08a50e737e0d1a727d987e98fcc568c83421e2c1
Matthew Bryant <matt@refinerylabs.io>
2019-08-13 08:03:34 +0000

    More changes to custom runtime

--- api/server.py
+++ api/server.py
@@ -5693,24 +5693,23 @@ def deploy_lambda( credentials, id, name, language, code, libraries, max_executi
 	# Add the custom runtime layer in all cases
 	if language == "nodejs8.10":
 		layers.append(
-			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-node810-custom-runtime:15"
+			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-node810-custom-runtime:17"
 		)
 	elif language == "php7.3":
 		layers.append(
-			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-php73-custom-runtime:15"
+			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-php73-custom-runtime:17"
 		)
 	elif language == "go1.12":
 		layers.append(
-			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-go112-custom-runtime:15"
+			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-go112-custom-runtime:17"
 		)
 	elif language == "python2.7":
 		layers.append(
-			#"arn:aws:lambda:us-west-2:134071937287:layer:refinery-python27-custom-runtime:15"
-			"arn:aws:lambda:us-west-2:561628006572:layer:python:17"
+			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-python27-custom-runtime:17"
 		)
 	elif language == "python3.6":
 		layers.append(
-			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-python36-custom-runtime:15"
+			"arn:aws:lambda:us-west-2:134071937287:layer:refinery-python36-custom-runtime:17"
 		)
 
 	deployed_lambda_data = yield local_tasks.deploy_aws_lambda(
