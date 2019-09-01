847689168e7d25b55fc8bf72ba78b9fd4ee77c57
Matthew Bryant <matt@refinerylabs.io>
2019-09-01 20:55:36 +0000

    Removed extra ruby stuffs

--- api/custom-runtime/ruby2.6.4/build.sh
+++ api/custom-runtime/ruby2.6.4/build.sh
@@ -5,9 +5,11 @@ cp runtime ./layer-contents/
 cp -r ../base-src/* ./layer-contents/
 cp -r ./ruby/ ./layer-contents/
 cp -r ./lib/ ./layer-contents/
-cp runtime ./layer-contents/ 
+cp runtime ./layer-contents/
 cd ./layer-contents/
 zip -r custom-runtime.zip *
 mv custom-runtime.zip ../
 cd ..
 rm -rf ./layer-contents/*
+aws s3 cp custom-runtime.zip s3://lambdapackagetestingbucket/ruby-2.6.4-custom-runtime.zip
+aws lambda publish-layer-version --layer-name refinery-ruby264-custom-runtime --description "Refinery Ruby 2.6.4 custom runtime layer." --content "S3Bucket=lambdapackagetestingbucket,S3Key=ruby-2.6.4-custom-runtime.zip" --compatible-runtimes "provided"
--- api/custom-runtime/ruby2.6.4/lib/lambda_logger.rb
+++
@@ -1,8 +0,0 @@
-class LambdaLogger
-  class << self
-    def log_error(exception:, message: nil)
-      STDERR.puts message if message
-      STDERR.puts JSON.pretty_unparse(exception.to_lambda_response)
-    end
-  end
-end
