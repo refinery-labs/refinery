16d5e045517b6ee497a6ef7900de765d27479f1b
Matthew Bryant <matt@refinerylabs.io>
2019-09-01 21:16:39 +0000

    Fixed build

--- api/custom-runtime/ruby2.6.4/build.sh
+++ api/custom-runtime/ruby2.6.4/build.sh
@@ -11,5 +11,3 @@ zip -r custom-runtime.zip *
 mv custom-runtime.zip ../
 cd ..
 rm -rf ./layer-contents/*
-aws s3 cp custom-runtime.zip s3://lambdapackagetestingbucket/ruby-2.6.4-custom-runtime.zip
-aws lambda publish-layer-version --layer-name refinery-ruby264-custom-runtime --description "Refinery Ruby 2.6.4 custom runtime layer." --content "S3Bucket=lambdapackagetestingbucket,S3Key=ruby-2.6.4-custom-runtime.zip" --compatible-runtimes "provided"
