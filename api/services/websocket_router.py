48f4a4f01cae9776550e0fbf07280b1af082011b
Matthew Bryant <matt@refinerylabs.io>
2019-09-17 19:59:55 +0000

    derp

--- api/services/websocket_router.py
+++ api/services/websocket_router.py
@@ -3,25 +3,6 @@ import tornado.websocket
 
 from utils.general import attempt_json_decode, logit
 
-def parse_websocket_message( input_message ):
-	"""
-	Returns the dictionary if it's a valid message and
-	returns False if it is not.
-	"""
-	message_contents = attempt_json_decode(
-		input_message
-	)
-	
-	# Reject it if it's not a dict
-	if type( message_contents ) != dict:
-		return False
-	
-	# Reject it if it doesn't contain a "debug_id"
-	if not "debug_id" in message_contents:
-		return False
-	
-	return message_contents
-
 """
 This is a dict which automatically routes messages from running
 Lambdas to the subscribing websocket connections.
@@ -112,5 +93,3 @@ class WebSocketRouter:
 				logit( "Cleared existing Refinery user websocket connection from WebSocket Router! (Debug ID: " + debug_id + ")" )
 			except ValueError:
 				pass
-		
-websocket_router = WebSocketRouter()
