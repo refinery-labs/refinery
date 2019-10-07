1222f1c83a9de23fd608f907ca915a2c020606c9
Free Wortley <00free@gmail.com>
2019-10-07 16:00:18 -0700

    Negative margins around code editor

--- front-end/src/components/RunLambda.tsx
+++ front-end/src/components/RunLambda.tsx
@@ -251,11 +251,15 @@ export default class RunLambda extends Vue implements RunLambdaProps {
           <Split props={{ direction: 'vertical' as Object }}>
             <SplitArea props={{ size: 40 as Object }}>
               <b-tabs nav-class="nav-justified" content-class="padding--none position--relative flex-grow--1">
-                <b-tab title="Input Data" active={true}>
-                  <div class="display--flex flex-grow--1 ace-hack">{renderEditorWrapper(null, inputDataEditor)}</div>
+                <b-tab title="Input Data" active={true} no-body={true}>
+                  <div class="display--flex flex-grow--1 ace-hack margin-left--negative-micro margin-right--negative-micro">
+                    {renderEditorWrapper(null, inputDataEditor)}
+                  </div>
                 </b-tab>
-                <b-tab title="Backpack Data">
-                  <div class="display--flex flex-grow--1 ace-hack">{renderEditorWrapper(null, backpackDataEditor)}</div>
+                <b-tab title="Backpack Data" no-body={true}>
+                  <div class="display--flex flex-grow--1 ace-hack margin-left--negative-micro margin-right--negative-micro">
+                    {renderEditorWrapper(null, backpackDataEditor)}
+                  </div>
                 </b-tab>
               </b-tabs>
             </SplitArea>
