8da0c3fd393088c89618d329b8df98f4e80f70cd
Matthew Bryant (mandatory) <matt@refinery.io>
2020-01-03 10:59:57 -0800

    Committing **just** the terraform changes

--- api/install/athena.tf
+++ api/install/athena.tf
@@ -1,6 +1,6 @@
 resource "aws_athena_database" "refinery_athena_db" {
 	name   = "refinery"
-	bucket = "${aws_s3_bucket.lambda-logging.id}"
+	bucket = aws_s3_bucket.lambda-logging.id
 }
 
 resource "aws_athena_workgroup" "refinery_workgroup" {
