3ddabd1a135eb2e702dea2def416d82688134def
Chris Thompson <chris@refinerylabs.io>
2020-02-11 20:35:42 -0800

    formatting

--- front-end/src/store/modules/project-view.ts
+++ front-end/src/store/modules/project-view.ts
@@ -1,4 +1,4 @@
-import {Module} from 'vuex';
+import { Module } from 'vuex';
 import uuid from 'uuid/v4';
 import LZString from 'lz-string';
 import {
@@ -21,8 +21,8 @@ import {
   WorkflowState,
   WorkflowStateType
 } from '@/types/graph';
-import {generateCytoscapeElements, generateCytoscapeStyle} from '@/lib/refinery-to-cytoscript-converter';
-import {CssStyleDeclaration, LayoutOptions} from 'cytoscape';
+import { generateCytoscapeElements, generateCytoscapeStyle } from '@/lib/refinery-to-cytoscript-converter';
+import { CssStyleDeclaration, LayoutOptions } from 'cytoscape';
 import cytoscape from '@/components/CytoscapeGraph';
 import {
   DeploymentViewActions,
@@ -31,8 +31,8 @@ import {
   ProjectViewMutators,
   UserActions
 } from '@/constants/store-constants';
-import {makeApiRequest} from '@/store/fetchers/refinery-api';
-import {API_ENDPOINT} from '@/constants/api-constants';
+import { makeApiRequest } from '@/store/fetchers/refinery-api';
+import { API_ENDPOINT } from '@/constants/api-constants';
 import {
   GetLatestProjectDeploymentRequest,
   GetLatestProjectDeploymentResponse,
@@ -63,25 +63,26 @@ import {
 } from '@/utils/project-helpers';
 import {
   availableTransitions,
-  blockTypeToImageLookup, DEFAULT_LANGUAGE_CODE,
+  blockTypeToImageLookup,
+  DEFAULT_LANGUAGE_CODE,
   demoModeBlacklist,
   savedBlockType
 } from '@/constants/project-editor-constants';
-import EditBlockPaneModule, {EditBlockActions, EditBlockGetters} from '@/store/modules/panes/edit-block-pane';
-import {createToast} from '@/utils/toasts-utils';
-import {ToastVariant} from '@/types/toasts-types';
+import EditBlockPaneModule, { EditBlockActions, EditBlockGetters } from '@/store/modules/panes/edit-block-pane';
+import { createToast } from '@/utils/toasts-utils';
+import { ToastVariant } from '@/types/toasts-types';
 import router from '@/router';
-import {deepJSONCopy} from '@/lib/general-utils';
-import EditTransitionPaneModule, {EditTransitionActions} from '@/store/modules/panes/edit-transition-pane';
-import {createShortlink, deployProject, openProject, teardownProject} from '@/store/fetchers/api-helpers';
-import {CyElements, CyStyle} from '@/types/cytoscape-types';
-import {addAPIBlocksToProject, createNewBlock, createNewTransition} from '@/utils/block-utils';
-import {saveEditBlockToProject} from '@/utils/store-utils';
+import { deepJSONCopy } from '@/lib/general-utils';
+import EditTransitionPaneModule, { EditTransitionActions } from '@/store/modules/panes/edit-transition-pane';
+import { createShortlink, deployProject, openProject, teardownProject } from '@/store/fetchers/api-helpers';
+import { CyElements, CyStyle } from '@/types/cytoscape-types';
+import { addAPIBlocksToProject, createNewBlock, createNewTransition } from '@/utils/block-utils';
+import { saveEditBlockToProject } from '@/utils/store-utils';
 import ImportableRefineryProject from '@/types/export-project';
-import {AllProjectsActions, AllProjectsGetters} from '@/store/modules/all-projects';
-import {kickOffLibraryBuildForBlocks} from '@/utils/block-build-utils';
-import {AddSharedFileArguments, AddSharedFileLinkArguments} from '@/types/shared-files';
-import {EditSharedFilePaneModule} from '@/store';
+import { AllProjectsActions, AllProjectsGetters } from '@/store/modules/all-projects';
+import { kickOffLibraryBuildForBlocks } from '@/utils/block-build-utils';
+import { AddSharedFileArguments, AddSharedFileLinkArguments } from '@/types/shared-files';
+import { EditSharedFilePaneModule } from '@/store';
 
 export interface ChangeTransitionArguments {
   transition: WorkflowRelationship;
@@ -416,7 +417,7 @@ const ProjectViewModule: Module<ProjectViewState, RootState> = {
       }
       state.openedProjectConfig = Object.assign({}, state.openedProjectConfig, {
         ...state.openedProjectConfig,
-        default_language: projectRuntimeLanguage,
+        default_language: projectRuntimeLanguage
       });
     },
 
@@ -513,7 +514,10 @@ const ProjectViewModule: Module<ProjectViewState, RootState> = {
       // Save new config to the backend
       await context.dispatch(ProjectViewActions.saveProjectConfig);
     },
-    async [ProjectViewActions.setProjectConfigRuntimeLanguage](context, projectConfigRuntimeLanguage: SupportedLanguage) {
+    async [ProjectViewActions.setProjectConfigRuntimeLanguage](
+      context,
+      projectConfigRuntimeLanguage: SupportedLanguage
+    ) {
       context.commit(ProjectViewMutators.setProjectRuntimeLanguage, projectConfigRuntimeLanguage);
 
       // Save new config to the backend
@@ -1498,7 +1502,7 @@ const ProjectViewModule: Module<ProjectViewState, RootState> = {
         let defaultLanguage = context.state.openedProjectConfig.default_language || SupportedLanguage.NODEJS_8;
         addBlockArgs.customBlockProperties = Object.assign({}, addBlockArgs.customBlockProperties, {
           language: defaultLanguage,
-          code: DEFAULT_LANGUAGE_CODE[defaultLanguage],
+          code: DEFAULT_LANGUAGE_CODE[defaultLanguage]
         });
       }
 
