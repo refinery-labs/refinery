bad5e159525e695dab95601b7197ec20d9bcaa8e
Free Wortley <00free@gmail.com>
2020-06-29 11:46:45 -0700

    Add copyright notice to files

--- api/custom-runtime/runtime/utils.py
+++ api/custom-runtime/runtime/utils.py
@@ -1,3 +1,18 @@
+
+"""
+Copyright (C) Refinery Labs Inc. - All Rights Reserved
+
+NOTICE: All information contained herein is, and remains
+the property of Refinery Labs Inc. and its suppliers,
+if any. The intellectual and technical concepts contained
+herein are proprietary to Refinery Labs Inc.
+and its suppliers and may be covered by U.S. and Foreign Patents,
+patents in process, and are protected by trade secret or copyright law.
+Dissemination of this information or reproduction of this material
+is strictly forbidden unless prior written permission is obtained
+from Refinery Labs Inc.
+"""
+
 from .exc import AlreadyInvokedException
 
 
