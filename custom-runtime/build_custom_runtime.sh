2ff051dd7ff71b343fb8523bcae50c95a705ed3e
Ian Kinsey <ian@aikbix.com>
2020-06-29 15:13:23 -0700

    Remove old base-src

--- custom-runtime/build_custom_runtime.sh
+++ custom-runtime/build_custom_runtime.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+rm -rf base-src
 virtualenv -p `which python2.7` env
 source env/bin/activate
 pip install -r requirements.txt
 python setup.py bdist_wheel
 unzip dist/custom_runtime-0.0.1-py2-none-any.whl -d dist
-cp -r env/lib/python2.7/site-packages runtime
-cp -r dist/custom_runtime dist/custom_runtime-0.0.1.dist-info runtime
-cp bootstrap runtime
+cp -r env/lib/python2.7/site-packages base-src
+cp -r dist/custom_runtime dist/custom_runtime-0.0.1.dist-info base-src
+cp bootstrap base-src
 deactivate
 rm -rf env dist build custom_runtime.egg-info
