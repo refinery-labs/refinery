e46dabaf351523a9cc39b3b80d5c3a9eaa9d3421
breadchris <chris@refinerylabs.io>
2020-07-07 17:21:45 -0700

    move code to a more common place

--- api/assistants/deployments/awslambda.py
+++ api/assistants/deployments/awslambda.py
@@ -1,17 +1,32 @@
-import random
-import time
-
 import pinject
 import tornado
-from tornado import gen
 
 from tornado.concurrent import run_on_executor, futures
-
-from botocore.exceptions import ClientError
 
 from tasks.aws_lambda import list_lambda_event_source_mappings_by_name
 from utils.general import log_exception, logit
 from utils.performance_decorators import emit_runtime_metrics
+
+from assistants.decorators import aws_exponential_backoff, RESOURCE_IN_USE_EXCEPTION, RESOURCE_NOT_FOUND_EXCEPTION
+
+
+@aws_exponential_backoff()
+def try_to_list_mappings(aws_client_factory, credentials, name):
+    return list_lambda_event_source_mappings_by_name(aws_client_factory, credentials, name)
+
+
+@aws_exponential_backoff(allowed_errors=[RESOURCE_IN_USE_EXCEPTION])
+def try_to_delete_event_source_mapping(lambda_client, mapping):
+    return lambda_client.delete_event_source_mapping(
+        UUID=mapping.uuid
+    )
+
+
+@aws_exponential_backoff(breaking_errors=[RESOURCE_NOT_FOUND_EXCEPTION])
+def try_to_delete_function(lambda_client, arn):
+    return lambda_client.delete_function(
+        FunctionName=arn,
+    )
 
 
 class LambdaManager(object):
@@ -41,36 +56,17 @@ class LambdaManager(object):
         # Cleanup the source mappings for when we recreate this lambda and they do not persist
         event_source_mappings = []
         if name is not None:
-            event_source_mappings = list_lambda_event_source_mappings_by_name(aws_client_factory, credentials, name)
+            event_source_mappings = try_to_list_mappings(aws_client_factory, credentials, name)
 
         for mapping in event_source_mappings:
-            attempts = 0
-            while attempts < 5:
-                try:
-                    lambda_client.delete_event_source_mapping(
-                        UUID=mapping.uuid
-                    )
-                except ClientError as e:
-                    if e.response["Error"]["Code"] != "ResourceInUseException":
-                        raise
+            try_to_delete_event_source_mapping(lambda_client, mapping)
 
-                    attempts += 1
-                    time.sleep(attempts * 2 + random.randint(0, 5))
-
-        was_deleted = False
-        try:
-            response = lambda_client.delete_function(
-                FunctionName=arn,
-            )
-            was_deleted = True
-        except ClientError as e:
-            if e.response["Error"]["Code"] != "ResourceNotFoundException":
-                raise
+        try_to_delete_function(lambda_client, arn)
 
         return {
             "id": id,
             "type": type,
             "name": name,
             "arn": arn,
-            "deleted": was_deleted,
+            "deleted": True,
         }
