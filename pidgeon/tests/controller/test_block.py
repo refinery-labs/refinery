c417a17af7cf8d607f739b539fe75c7655f56cdd
iakinsey <ian@refinery.io>
2020-07-16 16:53:04 -0700

    Use deployment id in auth token

--- pidgeon/tests/controller/test_block.py
+++ pidgeon/tests/controller/test_block.py
@@ -23,30 +23,29 @@ class TestBlock:
             for _ in range(100)
         ]
 
-        for user_uuid, execution_id, result_id, data in scenarios:
+        for deployment_id, execution_id, result_id, data in scenarios:
             auth = self._get_auth(
-                user_uuid,
-                execution_id,
+                deployment_id,
                 controller.config['block_result_key'],
                 controller.sym_crypto
             )
             data = str(uuid4())
             # Put
-            await controller.set_block_state(auth, user_uuid, execution_id, result_id, data)
+            await controller.set_block_state(auth, execution_id, result_id, data)
             # Get
-            result = await controller.get_block_state(auth, user_uuid, execution_id, result_id)
+            result = await controller.get_block_state(auth, execution_id, result_id)
 
             assert result == data
             # Delete
-            await controller.delete_block_state(auth, user_uuid, execution_id, result_id)
+            await controller.delete_block_state(auth, execution_id, result_id)
 
             # Get and fail
             with raises(Exception):
-                await controller.get_block_state(auth, user_uuid, execution_id, result_id)
+                await controller.get_block_state(auth, execution_id, result_id)
 
     def test_validate_key_mismatch(self, controller):
         pass
 
-    def _get_auth(self, user_uuid, execution_id, key, sym_crypto):
-        auth = {"user_uuid": user_uuid, "execution_id": execution_id}
+    def _get_auth(self, deployment_id, key, sym_crypto):
+        auth = {"deployment_id": deployment_id}
         return sym_crypto.encrypt(key, dumps(auth))
