bfd0af199191fd14e1a739bc441a31c92b89d19d
iakinsey <ian@refinery.io>
2020-07-17 16:37:21 -0700

    Add auth service

--- pidgeon/pidgeon/controller/block.py
+++ pidgeon/pidgeon/controller/block.py
@@ -8,31 +8,24 @@ class Block(Controller):
     config = None
     block_state = None
     sym_crypto = None
+    auth = None
+    _block_result_key = None
 
     @copy_args_to_public_fields
-    def __init__(self, config, block_state, sym_crypto):
-        self.key = config.get("block_result_key")
+    def __init__(self, config, block_state, sym_crypto, auth):
+        pass
 
     @handle("/block/state/get")
     async def get_block_state(self, auth, execution_id, result_id):
-        deployment_id = self._validate_key(auth)
+        deployment_id = await self.auth.authorize_block_result(auth)
         return await self.block_state.get_block_result(deployment_id, execution_id, result_id)
 
     @handle("/block/state/set")
     async def set_block_state(self, auth, execution_id, result_id, data):
-        deployment_id = self._validate_key(auth)
+        deployment_id = await self.auth.authorize_block_result(auth)
         await self.block_state.set_block_result(deployment_id, execution_id, result_id, data)
 
     @handle("/block/state/delete")
     async def delete_block_state(self, auth, execution_id, result_id):
-        deployment_id = self._validate_key(auth)
+        deployment_id = await self.auth.authorize_block_result(auth)
         await self.block_state.delete_block_result(deployment_id, execution_id, result_id)
-
-    def _validate_key(self, auth):
-        auth = loads(self.sym_crypto.decrypt(self.key, auth))
-        deployment_id = auth['deployment_id']
-
-        if not deployment_id:
-            raise AuthError("Unauthorized block storage access operation")
-
-        return deployment_id
