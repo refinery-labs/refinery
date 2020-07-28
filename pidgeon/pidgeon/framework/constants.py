9fa02c638bc06e513938564d135eddd28b42e223
iakinsey <ian@refinery.io>
2020-07-28 16:34:41 -0700

    Pidgeon Dockerfile

--- pidgeon/pidgeon/framework/constants.py
+++ pidgeon/pidgeon/framework/constants.py
@@ -5,4 +5,4 @@ from os.path import abspath, dirname, realpath, join
 PROJECT_ROOT = abspath(dirname(dirname(dirname(realpath(__file__)))))
 CONFIG_DIR = join(PROJECT_ROOT, "config")
 ENCODING = "UTF-8"
-ENV = environ.get("REFINERY_ENV", "production")
+ENV = environ.get("REFINERY_ENV", "common")
