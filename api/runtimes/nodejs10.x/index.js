c4210cbc358d5653f5dbbba958a544625326b1f7
Free Wortley <00free@gmail.com>
2020-10-27 12:15:59 -0700

    Fix Node Temporal runtime

--- api/runtimes/nodejs10.x/index.js
+++ api/runtimes/nodejs10.x/index.js
@@ -1,13 +1,14 @@
-const main = require("./refinery_main").main;
+const main = require('./refinery_main').main;
 
 exports.handler = async (event) => {
-   const result = await main(
-        event.hasOwnProperty("block_input") ? event['block_input'] : {},
-        event.hasOwnProperty("backpack") ? event['backpack'] : {}
-    );
-   const response = {
-     result: result,
-     backpack: backpack
-   };
-   return JSON.stringify(response)
+  const backpack = event.hasOwnProperty('backpack') ? event['backpack'] : {};
+  const result = await main(
+    event.hasOwnProperty('block_input') ? event['block_input'] : {},
+    backpack
+  );
+  const response = {
+    result: result,
+    backpack: backpack
+  };
+  return JSON.stringify(response)
 };
