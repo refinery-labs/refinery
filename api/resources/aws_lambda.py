eb14aea1d019b8a108b4c0aea5b22bee3ba2780f
iakinsey <ian@refinery.io>
2020-11-12 15:27:43 -0800

    Add some stuff to aws lambda

--- api/resources/aws_lambda.py
+++ api/resources/aws_lambda.py
@@ -2,4 +2,16 @@ from resources.base import Resource
 
 
 class AWSLambda(Resource):
-    pass
+    def __init__(self, app_config, aws_client_factory, credentials):
+        self.app_config = app_config
+        self.aws_client_factory = aws_client_factory
+        self.credentials = credentials
+
+    def serialize(self):
+        pass
+
+    def deploy(self):
+        pass
+
+    def teardown(self):
+        pass
