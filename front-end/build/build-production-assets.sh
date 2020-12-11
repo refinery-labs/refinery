48079404270cc20b8ec5745dc138da022f481a1a
breadchris <chris@refinerylabs.io>
2020-12-10 20:20:55 -0500

    test

--- front-end/build/build-production-assets.sh
+++ front-end/build/build-production-assets.sh
@@ -8,12 +8,14 @@ docker build -t front-end-image .
 
 # Run the container with a specific npm command and mount our output volume
 docker run \
+  -e NODE_ENV \
   -e AWS_DEFAULT_REGION \
   -e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI \
   -e CLOUDFRONT_URL \
   -e S3_DEPLOY_REGION \
   -e S3_DEPLOY_BUCKET \
   -e S3_DEPLOY_PATH \
-  -e APP_API_URL \
+  -e VUE_APP_API_HOST \
+  -e VUE_APP_STRIPE_PUBLISHABLE_KEY \
   -v "$(pwd)"/front-end-dist:/work/output \
   -t front-end-image build-and-copy
