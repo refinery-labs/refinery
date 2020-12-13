2424cb17f64707596d740d4009a9dcde88163e44
breadchris <chris@refinerylabs.io>
2020-12-13 03:54:27 -0500

    typo in handling app api host

--- front-end/build/replace-service-worker-paths.sh
+++ front-end/build/replace-service-worker-paths.sh
@@ -4,5 +4,7 @@
 # There is no way to replace the path for the service worker to be on NOT A CDN IN THE CONFIG
 # And because of this... The service worker breaks! So off to sed we go.
 # https://github.com/vuejs/vue-cli/issues/3571
-sed -i 's/https:\/\/d3asw1bke2pwdg.cloudfront.net\/manifest.json/\/manifest\/manifest.json/g' dist/index.html
-sed -i 's/https:\/\/d3asw1bke2pwdg.cloudfront.net\/precache-manifest/\/manifest\/precache-manifest/g' dist/service-worker.js
+sed -i 's/https:\/\/dixofz0e9vepn.cloudfront.net\/manifest.json/\/manifest\/manifest.json/g' dist/index.html
+sed -i 's/https:\/\/dixofz0e9vepn.cloudfront.net\/precache-manifest/\/manifest\/precache-manifest/g' dist/service-worker.js
+
+
