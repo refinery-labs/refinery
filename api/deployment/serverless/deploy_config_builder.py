a02402dffcc4d23fd2898386afcd5d45fed5d073
iakinsey <ian@refinery.io>
2021-01-26 15:04:28 -0800

    Complete baseline config builder

--- api/deployment/serverless/deploy_config_builder.py
+++ api/deployment/serverless/deploy_config_builder.py
@@ -1,3 +1,4 @@
+from copy import copy
 from functools import cached_property
 
 
@@ -6,20 +7,76 @@ class DeploymentConfigBuilder:
         self.project_id = project_id
         self.diagram_data = diagram_data
         self.lambda_resource_map = lambda_resource_map
-        self.deployment = {
-            "name": diagram_data['name'],
-            "project_id": project_id,
-            "global_handlers": {},
-            "workflow_relationships": [],
-            "workflow_states": []
-        }
 
     @cached_property
     def value(self):
-        return self.deployment
+        relationships = self.get_workflow_relationships()
+        states = self.get_workflow_state_mapper(relationships)
 
-    def add_workflow_relationship(self):
-        pass
+        return {
+            "name": diagram_data['name'],
+            "project_id": project_id,
+            "global_handlers": {},
+            "workflow_relationships": relationships,
+            "workflow_states": states
+        }
 
-    def add_workflow_state(self):
-        pass
+    def get_workflow_state_mapper(self, relationships):
+        states = {}
+
+        for workflow_state in self.diagram_data.get("workflow_states", []):
+            uuid = workflow_state['id']
+            base = self.get_deployed_workflow_state_base(workflow_state)
+            states[uuid] = base
+
+            if uuid in self.lambda_resource_map:
+                workflow_state['arn'] = self.lambda_resource_map[uuid]
+
+        return states
+
+    def get_deployed_workflow_state_base(self, workflow_state, relationships):
+        state = {
+            "transitions": {
+                "if": [],
+                "else": [],
+                "exception": [],
+                "then": []
+            },
+            **workflow_state
+        }
+
+        self.apply_transitions(state, relationships)
+
+        return state
+
+    def apply_transitions(self, state, relationships):
+        node_id = state['id']
+
+        for relationship in relationships:
+            rel_node_id = relationship['node']
+
+            if rel_node_id != node_id:
+                continue
+
+            type_ = relationship['type']
+            next_node_id = relationship['next']
+            state['transitions'][type_].append({
+                "arn": relationship['arn'],
+                "id": relationship['id'],
+                "name": relationship['name'],
+                'type': relationship['type'],
+                'node': self.lambda_resource_map.get(rel_node_id),
+                'next': self.lambda_resource_map.get(next_node_id)
+            })
+
+    def get_workflow_relationships(self):
+        relationships = []
+
+        for rel in self.diagram_data.get("workflow_relationships", []):
+            next_node_arn = self.lambda_resource_map.get(rel.get("next"))
+            relationship = {**rel}
+            relationship['arn'] = next_node_arn
+
+            relationships.append(relationship)
+
+        return relationships
